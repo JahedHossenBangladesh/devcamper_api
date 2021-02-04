0d078bdba721565cf8b722dac83b9129d48f6a64
JahedHossenBangladesh <jahedhossen4358.com>
2021-02-04 12:35:53 +0600

    in express get should send sendStatus and json

--- server.js
+++ server.js
@@ -5,5 +5,15 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.sendStatus = 200;
+  res.json({
+    name: "Jahed",
+  });
+});
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(
+  PORT,
+  console.log(`port is in ${process.env.NODE_ENV} or in ${PORT}`)
+);
